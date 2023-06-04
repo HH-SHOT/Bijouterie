cd3c841347150841da850a0902378a1bc0d064da
hamzaitlalachante <hamzaitlalachante@gmail.com>
2023-06-04 14:05:55 +0200

    go

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default async function Product() {
+    return (
+        <div>
+            <div>
+                <h1>Name</h1>
+                <p>Description</p>
+            </div>
+        </div>
+    )
+}
