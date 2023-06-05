fb0635965df00c3a02a9e046ef34203f7f06a101
hamzaitlalachante <hamzaitlalachante@gmail.com>
2023-06-06 01:56:43 +0200

    update

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,26 +1,50 @@
-'use client'
-import { Session } from "next-auth"
-import {signIn} from "next-auth/react"
-import Image from "next/image"
+"use client"
 
-export default function Nav (user){
-    return (
-        <nav className="flex justify-between items-center py-8">
-            <h1>
-                Bijouterie
-            </h1>
-            <ul className="flex items-center gap-12">
-                {!user  && (
-                    <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
-                        <button onClick={() => signIn()}>Sing in</button>
-                    </li>
-                )}
-                {user && (
-                 <li>
-                    <Image src={user?.image as string}alt={user.name as string} width={48} height={48} className="rounded-full" />
-                 </li>   
-                )}
-            </ul>
-        </nav>
-    )
+import { Session } from "next-auth"
+import { signIn } from "next-auth/react"
+import Image from "next/image"
+import Link from "next/link"
+import Cart from "./Cart"
+import { useCartStore } from "@/store"
+import { AiFillShopping } from "react-icons/ai"
+
+export default function Nav({ user }: Session) {
+  const cartStore = useCartStore()
+  return (
+    <nav className="flex justify-between items-center py-12">
+      <Link href={"/"}>
+        <h1>Bijouterie</h1>
+      </Link>
+      <ul className="flex items-center gap-12">
+        {/* Toggle the cart */}
+        <li
+          onClick={() => cartStore.toggleCart()}
+          className="flex items-center text-3xl relative cursor-pointer"
+        >
+          <AiFillShopping />
+          <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center">
+            {cartStore.cart.length}
+          </span>
+        </li>
+        {/* If the user is not signed in */}
+        {!user && (
+          <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
+            <button onClick={() => signIn()}>Sign in</button>
+          </li>
+        )}
+        {user && (
+          <li>
+            <Image
+              src={user?.image as string}
+              alt={user.name as string}
+              width={36}
+              height={36}
+              className="rounded-full"
+            />
+          </li>
+        )}
+      </ul>
+      {cartStore.isOpen && <Cart />}
+    </nav>
+  )
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,40 +1,38 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import { Stripe } from "stripe";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import Stripe from "stripe"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export const auth0options = {
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    //Add another provider
   ],
-    events:{
-        createUser: async ({user}) => {
-            const stripe: Stripe = new Stripe(process.env.STRIPE_API_KEY as string,{
-                apiVersion: '2022-11-15',
-            })
-         //Let's create a stripe customer
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      })
+      //Let's create a stripe customer
 
       const costumer = await stripe.customers.create({
-        email: user.email || undefined,
-        name: user.name || undefined,
+        email: user.email,
+        name: user.name,
       })
       //Also update our prisma user with the stripecustomerid
-
       await prisma.user.update({
         where: { id: user.id },
         data: { stripeCustomerId: costumer.id },
       })
-         
-    }
-    
+    },
+  },
 }
 
-}
-export default NextAuth(auth0options)
+export default NextAuth(authOptions)
