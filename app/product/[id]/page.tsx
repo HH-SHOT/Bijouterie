74aa3f69f7d0669c511c8c8bd4cc1fb29738798d
hamzaitlalachante <hamzaitlalachante@gmail.com>
2023-06-05 23:39:33 +0200

    .

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,12 @@
-export default async function Product() {
+import Image from "next/image"
+export default async function Product( {searchParams} ) {
     return (
-        <div>
+        <div className="flex justify-between gap-24 p-12 ">
+            <Image src={searchParams.image}
+            alt={searchParams.name}
+            width={600}
+            height={600}
+            />
             <div>
                 <h1>Name</h1>
                 <p>Description</p>
